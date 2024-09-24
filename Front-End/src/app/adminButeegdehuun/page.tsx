49c7baafb34b4315f8262ff455f5d2d8954e758f
24LP9083 24LP9083 <24LP9083@G301-CC0189.local>
2024-09-24 12:18:23 +0800

    ahhaha

--- Front-End/src/app/adminButeegdehuun/page.tsx
+++ Front-End/src/app/adminButeegdehuun/page.tsx
@@ -4,7 +4,12 @@ import { GrMoney } from "react-icons/gr";
 import { MdProductionQuantityLimits } from "react-icons/md";
 import { IoMdSettings } from "react-icons/io";
 import * as React from "react";
-
+import { Checkbox } from "@/Components/ui/checkbox";
+import Image from "next/image";
+import { FaShoppingBag } from "react-icons/fa";
+import { SlPencil } from "react-icons/sl";
+import { FaLightbulb } from "react-icons/fa";
+import { GiDropEarrings } from "react-icons/gi";
 import {
   Select,
   SelectContent,
@@ -18,11 +23,92 @@ import { Input } from "@/Components/ui/input";
 import { FaDollarSign } from "react-icons/fa";
 import { FaCalendarAlt } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
+import { MdOutlineHotelClass } from "react-icons/md";
+import { LuTrash } from "react-icons/lu";
 const adminButeegdehuun = () => {
+  const product = [
+    {
+      checkbox: "checkbox",
+      img: " <FaShoppingBag/>",
+      title: "Эмэгтэй цүнх",
+      price: "19'000₮",
+      number1: "76",
+      number2: "30",
+      day: "2024-01-10",
+      icon1: "<LuTrash/>",
+      icon2: " <SlPencil />",
+    },
+    {
+      checkbox: "checkbox",
+      img: " <FaLightbulb />",
+      title: "Гэрэл",
+      price: "29'000₮",
+      number1: "90",
+      number2: "28",
+      day: "2024-01-10",
+      icon1: "<LuTrash/>",
+      icon2: " <SlPencil />",
+    },
+    {
+      checkbox: "checkbox",
+      icon: " <FaShoppingBag/>",
+      title: "Эмэгтэй цүнх",
+      price: "19'000₮",
+      number1: "56",
+      number2: "30",
+      day: "2024-01-10",
+      icon1: "<LuTrash/>",
+      icon2: " <SlPencil />",
+    },
+    {
+      checkbox: "checkbox",
+      icon: " <GiDropEarrings />",
+      title: "Ээмэг",
+      price: "10'000₮",
+      number1: "16",
+      number2: "6",
+      day: "2024-01-10",
+      icon1: "<LuTrash/>",
+      icon2: " <SlPencil />",
+    },
+    {
+      checkbox: "checkbox",
+      icon: " <FaShoppingBag/>",
+      title: "Эмэгтэй цүнх",
+      price: "19'000₮",
+      number1: "36",
+      number2: "35",
+      day: "2024-01-10",
+      icon1: "<LuTrash/>",
+      icon2: " <SlPencil />",
+    },
+    {
+      checkbox: "checkbox",
+      icon: " <FaShoppingBag/>",
+      title: "Эмэгтэй цүнх",
+      price: "19'000₮",
+      number1: "95",
+      number2: "73",
+      day: "2024-01-10",
+      icon1: "<LuTrash/>",
+      icon2: " <SlPencil />",
+    },
+    {
+      checkbox: "checkbox",
+      icon: " <FaShoppingBag/>",
+      title: "Эмэгтэй цүнх",
+      price: "19'000₮",
+      number1: "206",
+      number2: "80",
+      day: "2024-01-10",
+      icon1: "<LuTrash/>",
+      icon2: " <SlPencil />",
+    },
+  ];
   return (
     <div className="w-1218px h-1000px flex flex-row justify-center mt-5">
-      <div className="w-[222px] h-[976px] border border-black">
-        <div className="w-[222px] h-[264px] border flex flex-col justify-between mt-5">
+      <div className="w-[222px] h-[976px] border bg-white">
+        <div className="w-[222px] h-[264px]  flex flex-col justify-between mt-5">
           <div className="flex h-[30px] gap-3 bg-white hover:bg-[#ededed]">
             <MdWindow className="w-[24px] h-[24px]" />
             <h1 className="font-bold">Хяналтын самбар</h1>
@@ -46,7 +132,7 @@ const adminButeegdehuun = () => {
         </div>
       </div>
       <div className="w-[1218px] h-[900px] border border-black bg-[#f7f7f8]">
-        <div className="w-[1218px] h-[56px] border border-black flex gap-3">
+        <div className="w-[1218px] h-[56px] border flex gap-3">
           <h1 className="font-bold text-[#3f4245] hover:text-black ml-5">
             Бүтээгдэхүүн
           </h1>
@@ -61,16 +147,17 @@ const adminButeegdehuun = () => {
           <div className="flex gap-3">
             <Select>
               <SelectTrigger className="w-[180px]">
+                <MdOutlineHotelClass className="w-[20px] h-[20px]" />
                 <SelectValue placeholder="Ангилал" />
               </SelectTrigger>
-              <SelectContent>
+              <SelectContent className="font-bold">
                 <SelectGroup>
-                  <SelectLabel>Fruits</SelectLabel>
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                  <SelectLabel>Class:</SelectLabel>
+                  <SelectItem value="apple">цүнх</SelectItem>
+                  <SelectItem value="banana">Гэрэл</SelectItem>
+                  <SelectItem value="blueberry">Гутал</SelectItem>
+                  <SelectItem value="grapes">Цаг</SelectItem>
+                  <SelectItem value="pineapple">Пүүз</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
@@ -79,14 +166,14 @@ const adminButeegdehuun = () => {
                 <FaDollarSign className="w-[20px] h-[20px]" />
                 <SelectValue placeholder="Үнэ" />
               </SelectTrigger>
-              <SelectContent>
+              <SelectContent className="font-bold">
                 <SelectGroup>
-                  <SelectLabel>Fruits</SelectLabel>
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                  <SelectLabel>Price:</SelectLabel>
+                  <SelectItem value="apple">10'000₮</SelectItem>
+                  <SelectItem value="banana">20'000₮</SelectItem>
+                  <SelectItem value="blueberry">30'000₮</SelectItem>
+                  <SelectItem value="grapes">40'000₮</SelectItem>
+                  <SelectItem value="pineapple">50'000₮</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
@@ -95,14 +182,14 @@ const adminButeegdehuun = () => {
                 <FaCalendarAlt className="w-[20px] h-[20px]" />
                 <SelectValue placeholder="Сараар" />
               </SelectTrigger>
-              <SelectContent>
+              <SelectContent className="font-bold">
                 <SelectGroup>
-                  <SelectLabel>Fruits</SelectLabel>
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                  <SelectLabel>Day:</SelectLabel>
+                  <SelectItem value="apple">1-2сар</SelectItem>
+                  <SelectItem value="banana">3-4сар</SelectItem>
+                  <SelectItem value="blueberry">5-6сар</SelectItem>
+                  <SelectItem value="grapes">7-8сар</SelectItem>
+                  <SelectItem value="pineapple">9-12сар</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
@@ -115,7 +202,32 @@ const adminButeegdehuun = () => {
             />
           </div>
         </div>
-        <div className="w-[1170px] h-[692px] border border-black"></div>
+        <div className="w-[1170px] h-[692px] mt-5 border-black ">
+          <div className="w-[1170px] h-[40px] ml-10 gap-20 flex font-bold">
+            <h1>Бүтээгдэхүүн</h1>
+            <h1>Ангилал</h1>
+            <h1>Үнэ</h1>
+            <h1>Үлдэгдэл</h1>
+            <h1>Зарагдсан</h1>
+            <h1>Нэмсэн огноо</h1>
+          </div>
+          {product.map((index, item) => {
+            return (
+              <div className="w-[1170px] h-[90px] border flex  flex-row  items-center ml-5 gap-20 ">
+                <Checkbox className="w-[20px] h-[20px]  border-black" />
+
+                <Image src="/Avatar.png" alt="avatar" width={40} height={40} />
+                <h1 className="font-bold">цүнх</h1>
+                <h1 className="font-bold">49'000₮</h1>
+                <h1 className="font-bold">70</h1>
+                <h1 className="ml-16 font-bold">49</h1>
+                <h1 className="ml-16 font-bold">2024-01-10</h1>
+                <LuTrash className="ml-10 text-red-500 hover:text-red-900 w-[20px] h-[20px]" />
+                <SlPencil className="text-green-400 w-[20px] h-[20px]" />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
